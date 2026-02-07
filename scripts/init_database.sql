2561252b65ad143ecde151f66b88ddbd0d14e4fc
mkr7o7 <mkr68044@gmail.com>
2026-02-07 12:23:57 +0530

    Update init_database.sql

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,1 +1,23 @@
-
+-- runinng this script will drop the entire datawarehouse database if it exists.
+--create database and scripts
+use master;
+go
+--drop and recreate the 'datawarehouse' database
+if exists (select 1 from sys.databases where name = 'datawarehouse')
+begin
+  alter database datawarehouse set single user with rollback immediate;
+  drop database datawarehouse;
+end;
+go
+--create the datawarehouse database
+create database datawarehouse;
+go
+use datawarehouse;
+go
+--create schemas
+create schema bronze;
+go
+create schema silver;
+go
+create schema gold;
+go
